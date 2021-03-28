b118d0e871cd0875990b3ddd917d68812f7df57f
Robert Grimes <rmzgrimes@gmail.com>
2021-03-28 13:42:31 -0500

    Added stubs for didOpen and definition

--- lib/PLS/Handler.pm
+++ lib/PLS/Handler.pm
@@ -10,8 +10,9 @@ sub new {
 	my $self = bless {}, $class;
 
 	$self->{supported_methods} = {
-		"initialize" => "initialize",
-		"initialized" => "initialized",
+		"initialize"              => "initialize",
+		"initialized"             => "initialized",
+		"textDocument/didOpen"    => "did_open",
 		"textDocument/definition" => "definition",
 	};
 
@@ -20,36 +21,64 @@ sub new {
 
 sub execute {
 	my $self = shift;
-	my $req = shift;
+	my $req  = shift;
 
-	if( my $method = $self->{supported_methods}->{$req->{method}}) {
-		return $self->$method($req->{params});
+	if ( my $method = $self->{supported_methods}->{ $req->{method} } ) {
+		return $self->$method( $req->{params} );
 	}
 }
 
 sub initialize {
 
-return {
-			result => {
-				capabilities => {
-					definitionProvider  => JSON::true,
-					declarationProvider => JSON::true,
-				}
+	return {
+		result => {
+			capabilities => {
+				textDocumentSync => {
+					openClose => JSON::true,
+					change    => 1,
+				},
+				definitionProvider  => JSON::true,
+				declarationProvider => JSON::true,
 			}
 		}
+	};
 }
 
 sub initialized {
 	return;
 }
 
-#{"textDocument":{"uri":"file:///home/buu/tmp/lsp/foo.test"},"position":{"line":0,"character":0}}
+#"params":{"textDocument":{"uri":"file:///home/buu/foo.test","languageId":"coctest","version":1,"text":"\n"}}
+sub did_open {
+	my $self   = shift;
+	my $params = shift;
+
+	#TODO Parse the $params->{text};
+
+	return;    #Notification, no output;
+}
+
+#{"textDocument":{"uri":"file:///home/buu/foo.test"},"position":{"line":0,"character":0}}
 sub definition {
+	my $self   = shift;
 	my $params = shift;
 
 	#TODO IMPLEMENT ME
-
-	return;
+	return {
+		result => {
+			uri   => "file:///foo",
+			range => {
+				start => {
+					line      => 1,
+					character => 2,
+				},
+				end => {
+					line      => 1,
+					character => 4,
+				}
+			}
+		}
+	};
 
 }
 
